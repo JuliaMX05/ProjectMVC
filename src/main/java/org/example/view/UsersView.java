6064e917d5627f3a93f9b81f6e2afcf40af6f2fc
Sasha Romanov <sasharomanov@192.168.88.236>
2023-03-02 19:26:34 +0300

    Initial commit

---
+++ src/main/java/org/example/view/UsersView.java
@@ -0,0 +1,37 @@
+package org.example.view;
+
+import org.example.bean.User;
+import org.example.controller.Controller;
+import org.example.model.ModelData;
+
+public class UsersView implements View {
+
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All " + (modelData.isDisplayDeletedUserList() ? "deleted " : "") + "users:");
+        for (User user : modelData.getUsers()) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+
+    public void fireEventOpenUserEditForm(long id) {
+        controller.onOpenUserEditForm(id);
+    }
+
+}
